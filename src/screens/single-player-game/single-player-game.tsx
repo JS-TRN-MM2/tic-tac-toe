d04e0923d8c9b195748bb9732da563f67c5d5bb6
Missy Maloney <missymaloney1@gmail.com>
2021-08-27 15:06:09 -0400

    added initial screens and utility functions

---
+++ src/screens/single-player-game/single-player-game.tsx
@@ -0,0 +1,28 @@
+import React, { ReactElement } from "react";
+import { SafeAreaView } from "react-native";
+import { GradientBackground } from "@components";
+import styles from "./single-player-game.styles";
+import { Board } from "@components";
+import { printFormattedBoard, isEmpty, isFull, getAvailableMoves, BoardState } from "@utils";
+
+export default function Game(): ReactElement {
+    const b: BoardState = ["x", "o", "o", "o", "x", "x", "o", "x", "o"];
+
+    printFormattedBoard(b);
+    console.log(isEmpty(b));
+    console.log(isFull(b));
+    console.log(getAvailableMoves(b));
+    return (
+        <GradientBackground>
+            <SafeAreaView style={styles.container}>
+                <Board
+                    onCellPressed={index => {
+                        alert(index);
+                    }}
+                    state={b}
+                    size={300}
+                />
+            </SafeAreaView>
+        </GradientBackground>
+    );
+}
